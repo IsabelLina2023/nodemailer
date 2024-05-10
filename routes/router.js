950535109f32e5ccdfa3a445df1519208c56258e
Isabel <normarojasg@gmail.com>
2024-05-09 20:24:13 -0400

    actualizaciones

--- routes/router.js
+++ routes/router.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import enviar from '../config/mailer.js';
+
 
 const router = express.Router();
 
@@ -6,7 +8,11 @@ router.get('/inicio', (req, res) =>{
     res.send('hola mundo, soy la configuración con express');
 });
 
-
+router.get('/enviar', (req, res)=>{
+    const {to, subject, html} = req.query;
+    enviar(to, subject, html);
+    res.send('Correo enviado');
+})
 
 
 export default router;
